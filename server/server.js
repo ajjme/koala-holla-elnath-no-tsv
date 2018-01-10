716c60b2ca338d6682394ad8f6eb17bb34de2736
Andrew Johnson <ajj@byu.net>
2018-01-10 10:30:13 -0600

    Database populated, PG installed

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
+
 // Start listening for requests on a specific port
 app.listen(port, function(){
   console.log('listening on port', port);
